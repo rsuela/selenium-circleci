c03671ec696630af93333d8a8b723fe773a2698d
unknown <rsuela@gmail.com>
2018-02-12 22:16:32 +0800

    finalize testcase

--- tests/common.py
+++ tests/common.py
@@ -21,16 +21,18 @@ class common_functionalities:
         data={'action':'set_score', 'score':result})
     
     def login(self, driver, username, password):
-        print "Loging using %s" %(username)
+        print "- Login using %s" %(username)
         driver.find_element_by_xpath("//header[@id='header']/div/div/div/i").click()
         driver.find_element_by_link_text("Log in").click()
+        print "-- email address %s" %(username)
         driver.find_element_by_id("email_address").send_keys(username)
+        print "-- password %s" %(password)
         driver.find_element_by_id("user_pass").send_keys(password)
         driver.find_element_by_xpath("//form[@id='loginform']/div/div/div[1]/label").click()
         driver.find_element_by_id("wp_submit").click()
     
     def logout(self, driver):
-        print "Logging out"
+        print "- Logging out"
         driver.find_element_by_xpath("//header[@id='header']/div/div/div/i").click()
         
         driver.find_element_by_link_text("Log out").click()
@@ -38,32 +40,44 @@ class common_functionalities:
             driver.find_element_by_link_text("log out").click()
 
     def change_price_pkg(self, driver, pkg):
-        print "Changing package price"
+        print "- Changing package price"
         driver.get(pkg['PKGURL'])
         
         if len(driver.find_elements_by_xpath("//form[@name='add_new_package']/table")) > 0:
             time.sleep(2)
-        
+        print "-- USD: %s" %(pkg['USD'])
         driver.find_element_by_name("edit_package_price").clear()
         driver.find_element_by_name("edit_package_price").send_keys(pkg['USD'])
-
+        print "-- AUD: %s" %(pkg['AUD'])
         driver.find_element_by_name("edit_aud_package_price").clear()
         driver.find_element_by_name("edit_aud_package_price").send_keys(pkg['AUD'])
-
+        print "-- NZD: %s" %(pkg['NZD'])
         driver.find_element_by_name("edit_nzd_package_price").clear()
         driver.find_element_by_name("edit_nzd_package_price").send_keys(pkg['NZD'])
-
+        print "-- CAD: %s" %(pkg['CAD'])
         driver.find_element_by_name("edit_cad_package_price").clear()
         driver.find_element_by_name("edit_cad_package_price").send_keys(pkg['CAD'])
-
+        print "-- EUR: %s" %(pkg['EUR'])
         driver.find_element_by_name("edit_eur_package_price").clear()
         driver.find_element_by_name("edit_eur_package_price").send_keys(pkg['EUR'])
-
+        print "-- GBP: %s" %(pkg['GBP'])
         driver.find_element_by_name("edit_gbp_package_price").clear()
         driver.find_element_by_name("edit_gbp_package_price").send_keys(pkg['GBP'])
-        
+        print "-- update complete"
         driver.find_element_by_xpath("//table[@id='price_fields']/tbody/tr[2]/td[1]/h3").click()
         driver.find_element_by_xpath("//table[@id='price_fields']/tbody/tr[3]/td[2]/button").click()
         
     def read_email():
         print "Accessing email account"
+        
+    def navigate_to_clients(self, driver, search):
+        client_info = {}
+        current_url = driver.current_url
+        driver.get(current_url + "admin.php?page=client-users")
+        driver.find_element_by_xpath("//div[@id='example_filter']/label/input").send_keys(search)
+        for client in driver.find_elements_by_xpath("//table[@id='example']/tbody/tr"):
+            client_info['name'] = client.find_element_by_xpath(".//td[1]").text.encode('ascii', 'ignore')
+            client_info['email'] = client.find_element_by_xpath(".//td[2]").text.encode('ascii', 'ignore')
+            client_info['balance'] = client.find_element_by_xpath(".//td[3]").text.encode('ascii', 'ignore')
+        
+        return client_info
--- tests/public_user_credit_purchases.py
+++ tests/public_user_credit_purchases.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.support.ui import Select
 from configuration import config as cfg
 from appium import webdriver
-from testdata import data as td
+from public_user_credit_purchases_testdata import data as td
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -34,7 +34,9 @@ class PublicUserPurchaseTransactions(unittest.TestCase):
     def test_validate_usd_purchase_pkg1(self):
         '''Validate public user purchase with USD currency and using VISA credit card'''
         try:
-            # We need to make sure that price packages are set based on test data
+            ## We need to make sure that price packages are set based on test data
+            print "\n Testcase: Public user purchases USD package1"
+            print " Amount to purchase: %s\n"%(td.test_pkg1['USD'])
             self.driver.get("%s"%td.test_url)
             self.cf.login(self.driver, td.test_admin_username, td.test_admin_password)
             self.cf.change_price_pkg(self.driver, td.test_pkg1)
@@ -50,25 +52,25 @@ class PublicUserPurchaseTransactions(unittest.TestCase):
                 time.sleep(2)
             
             package1 = self.driver.find_element_by_xpath("//div[@class='modal-content']/div[@class='modal-body']/div/div[1]/a")
-            print "Assert package \nExpected: %s %s \nLive: %s" %(td.test_pkg1['USD'], "USD", package1.text.encode('ascii', 'ignore'))
+            print "- Assert package \n-- Expected: %s %s \n-- Live: %s" %(td.test_pkg1['USD'], "USD", package1.text.encode('ascii', 'ignore'))
             self.assertEqual("%s %s"%(td.test_pkg1['USD'], "USD"), package1.text.encode('ascii', 'ignore'))
             self.cf.screenshot(self.api_session, self.id, "Package1 Dialog")
             
             package1.click()
             self.cf.screenshot(self.api_session, self.id, "Checkout Page")
             
-            # Checkout page
-            print "Checkout page"
-            print "First name: %s"%(td.user1['name'])
+            ## Checkout page
+            print "- Checkout page"
+            print "-- First name: %s"%(td.user1['name'])
             self.driver.find_element_by_id("user_first_name").send_keys(td.user1['name'])
-            print "Surname: %s"%(td.user1['surname'])
+            print "-- Surname: %s"%(td.user1['surname'])
             self.driver.find_element_by_id("user_last_name").send_keys(td.user1['surname'])
-            print "Email Address: %s"%(td.user1['email'])
+            print "-- Email Address: %s"%(td.user1['email'])
             self.driver.find_element_by_id("email_address").send_keys(td.user1['email'])
             self.cf.screenshot(self.api_session, self.id, "Email Address")
             self.driver.find_element_by_id("email_re").send_keys(td.user1['email'])
             self.cf.screenshot(self.api_session, self.id, "Confirm Email Address")
-            print "Mobile: %s"%(td.user1['mobile'])
+            print "-- Mobile: %s"%(td.user1['mobile'])
             self.driver.find_element_by_id("phoneNumber").send_keys(td.user1['mobile'])
             self.cf.screenshot(self.api_session, self.id, "Checkout Page with details")
             
@@ -76,7 +78,7 @@ class PublicUserPurchaseTransactions(unittest.TestCase):
             self.driver.find_element_by_id("countries_timezones2").click()
             for country in self.driver.find_elements_by_xpath("//div[@class='bfh-selectbox-options']/div/ul/li/a"):
                 if country.text.encode('ascii', 'ignore').lower() == td.user1['country'].lower():
-                    print "Selected %s"%(country.text.encode('ascii', 'ignore').lower())
+                    print "-- Selected %s"%(country.text.encode('ascii', 'ignore').lower())
                     country.click()
                     time.sleep(1) # to populate the timezone
                     break
@@ -84,7 +86,7 @@ class PublicUserPurchaseTransactions(unittest.TestCase):
             self.driver.find_element_by_id("timezone").click()
             for timezone in self.driver.find_elements_by_xpath("//div[@id='timezone']/div[@class='bfh-selectbox-options']/div/ul/li/a"):
                 if td.user1['timezone'].lower() in timezone.text.encode('ascii', 'ignore').lower():
-                    print "Selected %s"%(timezone.text.encode('ascii', 'ignore'))
+                    print "-- Selected %s"%(timezone.text.encode('ascii', 'ignore'))
                     timezone.click()
                     break
             self.cf.screenshot(self.api_session, self.id, "Checkout Page with details")
@@ -94,16 +96,28 @@ class PublicUserPurchaseTransactions(unittest.TestCase):
             self.driver.find_element_by_id("card_name").send_keys("%s %s"%(td.user1['name'], td.user1['surname']))
             self.driver.find_element_by_id("cvv").send_keys(td.user1['cvv'])
             self.driver.find_element_by_id("terms").click()
-            print "Submitting form"
+            print "-- Submitting form"
             self.driver.find_element_by_id("check_out_buy_now_button").click()
             
             self.cf.wait_till_visible(self.driver.find_elements_by_id("checkoutResultSucess"))
             result_msg = "Thank you for your purchase. Please check your email inbox for further details."
-            print "Result Message: %s"%(self.driver.find_element_by_xpath("//span[@id='Seamless_ReportBugForm_expected_results']/span[@class='seamless-view msgForNonLoggedIn']").text.encode('ascii', 'ignore'))
+            print "- Result Message: %s"%(self.driver.find_element_by_xpath("//span[@id='Seamless_ReportBugForm_expected_results']/span[@class='seamless-view msgForNonLoggedIn']").text.encode('ascii', 'ignore'))
             self.assertEqual(result_msg, self.driver.find_element_by_xpath("//span[@id='Seamless_ReportBugForm_expected_results']/span[@class='seamless-view msgForNonLoggedIn']").text.encode('ascii', 'ignore'))
             
             # go to admin and check balance
-            
+            print '\n- Verification. Going to Client List'
+            self.driver.get("%s"%td.test_url)
+            self.cf.login(self.driver, td.test_admin_username, td.test_admin_password)
+            client_info = self.cf.navigate_to_clients(self.driver, "%s %s"%(td.user1['name'], td.user1['surname']))
+            print "-- Client Name: %s"%(client_info['name'])
+            print "-- Email Address: %s"%(client_info['email'])
+            print "-- Balance: %s"%(client_info['balance'])
+            print "-- Assert balance \n-- Expected: %s \n-- Live: %s" %("%.2f%s"%(float(td.test_pkg1['USD']),"USD"), client_info['balance'])
+            self.assertEqual("%.2f%s"%(float(td.test_pkg1['USD']),"USD"), client_info['balance'])
+            self.cf.screenshot(self.api_session, self.id, "Verification public user purchase")
+            self.driver.get("%s"%td.test_url)
+            self.cf.logout(self.driver)
+            print "- Testcase passed."
             self.test_result = 'pass'
 
         except AssertionError as e:
